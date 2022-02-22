cf3a7a4078b8bd8208ffc5c74101e9dd44c1ad69
Philip Salqvist <philipsalqvist@MacBook-Pro-som-tillhor-Philip.local>
2022-02-22 11:07:08 +0100

    feat: add test cases to replaceNodeWithNode
    fixes #39

--- test/com/jwetherell/algorithms/data_structures/test/BinarySearchTreeTests.java
+++ test/com/jwetherell/algorithms/data_structures/test/BinarySearchTreeTests.java
@@ -1,5 +1,6 @@
 package com.jwetherell.algorithms.data_structures.test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Collection;
@@ -9,6 +10,7 @@ import org.junit.Test;
 
 import com.jwetherell.algorithms.data_structures.BinarySearchTree;
 import com.jwetherell.algorithms.data_structures.BinarySearchTree.DepthFirstSearchOrder;
+import com.jwetherell.algorithms.data_structures.BinarySearchTree.Node;
 import com.jwetherell.algorithms.data_structures.test.common.JavaCollectionTest;
 import com.jwetherell.algorithms.data_structures.test.common.TreeTest;
 import com.jwetherell.algorithms.data_structures.test.common.Utils;
@@ -139,5 +141,205 @@ public class BinarySearchTreeTests {
         }
         bst.getDFS(DepthFirstSearchOrder.postOrder);
     }
+
+    /**
+     * replace root
+     */
+    @Test
+    public void testReplace1() {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+
+        Integer[] data = new Integer[]{7, 2, 3, 4, 5};
+        for(int i : data) {
+            bst.add(i);
+        }
+
+        Node<Integer> remove = bst.getNode(7);
+        Node<Integer> replacement = new Node<Integer>(null, 6);
+
+        bst.replaceNodeWithNode(remove, replacement);
+
+        assertTrue(bst.contains(6));
+        assertFalse(bst.contains(7));
+    }
+
+    /**
+     * `replacementNode` is null
+     */
+    @Test
+    public void testReplace2() {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+
+        Integer[] data = new Integer[]{1, 2, 3, 4, 5};
+        for(int i : data) {
+            bst.add(i);
+        }
+
+        Node<Integer> remove = bst.getNode(1);
+        Node<Integer> replacement = null;
+
+        bst.replaceNodeWithNode(remove, replacement);
+
+        assertFalse(bst.contains(1));
+    }
+
+    /**
+     * `replacementNode` is null
+     * `removeNode` left child of parent
+     */
+    @Test
+    public void testReplace3() {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+
+        Integer[] data = new Integer[]{2, 1, 3, 4, 5};
+        for(int i : data) {
+            bst.add(i);
+        }
+
+        Node<Integer> remove = bst.getNode(1);
+        Node<Integer> replacement = null;
+
+        bst.replaceNodeWithNode(remove, replacement);
+
+        assertFalse(bst.contains(1));
+    }
+
+    /**
+     * `replacementNode` is null
+     * `removeNode` right child of parent
+     */
+    @Test
+    public void testReplace4() {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+
+        Integer[] data = new Integer[]{2, 3, 4, 5};
+        for(int i : data) {
+            bst.add(i);
+        }
+
+        Node<Integer> remove = bst.getNode(3);
+        Node<Integer> replacement = null;
+
+        bst.replaceNodeWithNode(remove, replacement);
+
+        assertFalse(bst.contains(3));
+    }
+
+    /**
+     * add parent to `replacementNode`
+     * `replacementNode` is right side of parent
+     * `replacementNode` have non null children
+     */
+    @Test
+    public void testReplace5() {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+
+        Integer[] data = new Integer[]{13, 14, 3, 4, 1};
+        for(int i : data) {
+            bst.add(i);
+        }
+
+        Node<Integer> remove = bst.getNode(14);
+        BinarySearchTree<Integer> bst2 = new BinarySearchTree<Integer>();
+        bst2.add(7);
+        bst2.add(15);
+        bst2.add(16);
+        bst2.add(8);
+        Node<Integer> replacement = bst2.getNode(15);
+
+
+        bst.replaceNodeWithNode(remove, replacement);
+        
+        assertFalse(bst.contains(14));
+        assertTrue(bst.contains(15));
+    }
+
+    /**
+     * add parent to `replacementNode`
+     * `replacementNode` is right side of parent
+     * `replacementNode` has a null left child
+     */
+    @Test
+    public void testReplace6() {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+
+        Integer[] data = new Integer[]{13, 14, 3, 4, 1};
+        for(int i : data) {
+            bst.add(i);
+        }
+
+        Node<Integer> remove = bst.getNode(14);
+        BinarySearchTree<Integer> bst2 = new BinarySearchTree<Integer>();
+        bst2.add(7);
+        bst2.add(15);
+        bst2.add(16);
+        Node<Integer> replacement = bst2.getNode(15);
+
+
+        bst.replaceNodeWithNode(remove, replacement);
+        
+        assertFalse(bst.contains(14));
+        assertTrue(bst.contains(15));
+    }
+
+    /**
+     * add parent to `replacementNode`
+     * `replacementNode` so it is left child of parent
+     * `replacementNode` have non null children
+     */
+    @Test
+    public void testReplace7() {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+
+        Integer[] data = new Integer[]{13, 3, 4, 1};
+        for(int i : data) {
+            bst.add(i);
+        }
+
+        Node<Integer> remove = bst.getNode(3);
+        BinarySearchTree<Integer> bst2 = new BinarySearchTree<Integer>();
+        bst2.add(9);
+        bst2.add(6);
+        bst2.add(4);
+        bst2.add(8);
+        Node<Integer> replacement = bst2.getNode(6);
+
+
+        bst.replaceNodeWithNode(remove, replacement);
+
+        assertTrue(bst.contains(6));
+        assertFalse(bst.contains(3));
+    }
+
+    /**
+     * add parent to `replacementNode`
+     * `replacementNode` so it is left child of parent
+     * `replacementNode' has a null right child
+     */
+    @Test
+    public void testReplace8() {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+
+        Integer[] data = new Integer[]{13, 3, 4, 1};
+        for(int i : data) {
+            bst.add(i);
+        }
+
+        Node<Integer> remove = bst.getNode(3);
+        BinarySearchTree<Integer> bst2 = new BinarySearchTree<Integer>();
+        bst2.add(9);
+        bst2.add(6);
+        bst2.add(4);
+        Node<Integer> replacement = bst2.getNode(6);
+
+
+        bst.replaceNodeWithNode(remove, replacement);
+
+        assertTrue(bst.contains(6));
+        assertFalse(bst.contains(3));
+    }
+
+
+    
 
 }
